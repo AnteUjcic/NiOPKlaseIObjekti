857539728da8917855332c4fce2389c8fed19205
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2021-03-21 19:29:25 +0100

    Add files via upload

---
+++ NiOPKlaseIObjekti3/KlasaX.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiOPKlaseIObjekti3
+{
+    class KlasaX
+    {
+        public int[] array { get; set; } = new int[100];
+        public int index { get; set; }
+
+    }
+}
