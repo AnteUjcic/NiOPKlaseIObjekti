34e63c803c330bb5cd472fafe01dafbf05f20f30
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2021-03-21 19:29:37 +0100

    Add files via upload

---
+++ NiOPKlaseIObjekti3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiOPKlaseIObjekti3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaX x = new KlasaX();
+            for (x.index = 0; x.index < 100; x.index++)
+            {
+                x.array[x.index] = x.index + 1;
+            }
+            Console.WriteLine("50. broj u polju array = " + x.array[49]);
+            
+
+
+        }
+    }
+}
